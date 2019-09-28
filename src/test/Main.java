b40a4307b10f031c721b96bcd5e683beaf55268c
mrx <892484083@qq.com>
2019-09-28 21:54:54 +0800

    新增java项目

---
+++ src/test/Main.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C), 2019-2019, 微元素公司
+ * FileName: Main
+ * Author:   xzm
+ * Date:     2019/9/28 21:51
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+package test;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author xzm
+ * @create 2019/9/28
+ * @since 1.0.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("4444");
+    }
+}
