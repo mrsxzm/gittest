05199598805efb0cfff6f15ecbee72723cdfcfa9
mrx <892484083@qq.com>
2019-09-28 22:03:24 +0800

    ee

--- src/test/Main.java
+++ src/test/Main.java
@@ -21,6 +21,12 @@ package test;
  */
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("4444");
+        System.out.println("4444");
+        System.out.println("4444");
+        System.out.println("4444");
+        System.out.println("4444");
         System.out.println("4444");
     }
 }
